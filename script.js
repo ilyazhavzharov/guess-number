0feaa9e2f261415b227136b79c37726846ab4ada
ILYA ZHAVZHAROV <73776583+ilyazhavzharov@users.noreply.github.com>
2023-10-12 22:47:17 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-/*
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = '🎉 Correct Number!';
-
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
-
-document.querySelector('.guess').value = 23;
-console.log(document.querySelector('.guess').value);
-*/
-
 let secretNumber = Math.trunc(Math.random() * 20 + 1);
 let score = 20;
 let hightScore = 0;
@@ -48,28 +37,6 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = 0;
     }
   }
-  //   // When guess is too high
-  // } else if (guess > secretNumber) {
-  //   if (score > 1) {
-  //     document.querySelector('.message').textContent = '📈 Too high!';
-  //     score--;
-  //     document.querySelector('.score').textContent = score;
-  //   } else {
-  //     document.querySelector('.message').textContent = '💥 You lost the game!';
-  //     document.querySelector('.score').textContent = 0;
-  //   }
-
-  //   // When guess is too low
-  // } else if (guess < secretNumber) {
-  //   if (score > 1) {
-  //     document.querySelector('.message').textContent = '📉 Too low!';
-  //     score--;
-  //     document.querySelector('.score').textContent = score;
-  //   } else {
-  //     document.querySelector('.message').textContent = '💥 You lost the game!';
-  //     document.querySelector('.score').textContent = 0;
-  //   }
-  // }
 });
 
 // WHEN CLICK ON "AGAIN" BUTTON
@@ -85,6 +52,41 @@ document.querySelector('.again').addEventListener('click', function () {
   document.querySelector('.number').style.width = '15rem';
 });
 
+
+//BAD CODE / OLD CODE
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
+
+// document.querySelector('.guess').value = 23;
+// console.log(document.querySelector('.guess').value);
+
+// } else if (guess > secretNumber) {
+//   if (score > 1) {
+//     document.querySelector('.message').textContent = '📈 Too high!';
+//     score--;
+//     document.querySelector('.score').textContent = score;
+//   } else {
+//     document.querySelector('.message').textContent = '💥 You lost the game!';
+//     document.querySelector('.score').textContent = 0;
+//   }
+
+//   // When guess is too low
+// } else if (guess < secretNumber) {
+//   if (score > 1) {
+//     document.querySelector('.message').textContent = '📉 Too low!';
+//     score--;
+//     document.querySelector('.score').textContent = score;
+//   } else {
+//     document.querySelector('.message').textContent = '💥 You lost the game!';
+//     document.querySelector('.score').textContent = 0;
+//   }
+// }
+
+
+// TASKS
 // 1. Select the element with the 'again' class and attach a click event handler
 // 2. In the handler function, restore initial values of the score and secretNumber variables
 // 3. Restore the initial conditions of the message, number, score and guess input field
